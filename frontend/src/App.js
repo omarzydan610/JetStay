2eb5666834645a64ebe5410b195b59b49b801c66
omarzydan610 <omarzydan610@gmail.com>
2025-11-04 22:22:36 +0200

    Initialize Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
