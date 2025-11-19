3c12fde7038430b1342513df999a98551d193fe8
omarzydan610 <omarzydan610@gmail.com>
2025-11-19 20:47:39 +0200

    feat: create reposetories

---
+++ backend/src/main/java/com/example/backend/repository/RoomTypeRepository.java
@@ -0,0 +1,11 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RoomTypeRepository extends JpaRepository<RoomType, Integer> {
+
+}
