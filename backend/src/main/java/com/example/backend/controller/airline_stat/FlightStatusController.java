64914fdf1d00d7865d4f9171e4412c01ca55545d
AbdElrahman Elsayed <abdonawar1234567@gmail.com>
2025-11-27 02:51:29 +0200

    refactor: update FlightStatusController and FlightStatusService to utilize JWT for airline ID retrieval

--- backend/src/main/java/com/example/backend/controller/airline_stat/FlightStatusController.java
+++ backend/src/main/java/com/example/backend/controller/airline_stat/FlightStatusController.java
@@ -1,10 +1,15 @@
 package com.example.backend.controller.airline_stat;
 
 import com.example.backend.service.airline_stat.FlightStatusService;
+
+import io.jsonwebtoken.Claims;
+
 import com.example.backend.dto.AirlineDTO.FlightStatusRequestDTO;
 import com.example.backend.dto.response.SuccessResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,16 +19,17 @@ public class FlightStatusController {
     @Autowired
     private FlightStatusService flightStatusService;
 
-    @GetMapping("/{airlineName}")
-    public ResponseEntity<SuccessResponse<FlightStatusRequestDTO>> getFlightStatus(
-            @PathVariable(required = false) String airlineName) {
-        FlightStatusRequestDTO data = flightStatusService.getTicketsAndFlightSummary(airlineName);
+    @GetMapping("/")
+    public ResponseEntity<SuccessResponse<FlightStatusRequestDTO>> getFlightStatus() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Claims claims = (Claims) auth.getDetails();
+        FlightStatusRequestDTO data = flightStatusService.getTicketsAndFlightSummary(claims.get("airline_id", Integer.class));
         return ResponseEntity.ok(SuccessResponse.of("Flight status retrieved successfully", data));
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<SuccessResponse<FlightStatusRequestDTO>> getAllFlightsStatus() {
-        FlightStatusRequestDTO data = flightStatusService.getTicketsAndFlightSummary(null);
+        FlightStatusRequestDTO data = flightStatusService.getTicketsAndFlightSummary();
         return ResponseEntity.ok(SuccessResponse.of("Flight status retrieved successfully", data));
     }
 }
