9b05d546fe6ad76666cc89616d019f7d5a09e77c
AbdElrahman Elsayed <abdonawar1234567@gmail.com>
2025-12-10 03:27:36 +0200

    fix the bug of the crush when enter from the login

--- frontend/src/pages/HomePage/BaseHomePage.jsx
+++ frontend/src/pages/HomePage/BaseHomePage.jsx
@@ -15,8 +15,8 @@ function BaseHomePage() {
     const fetchData = async () => {
       if (
         !userData ||
-        ((userData.Role === "HOTEL_ADMIN" ||
-          userData.Role === "AIRLINE_ADMIN") &&
+        ((userData?.Role === "HOTEL_ADMIN" ||
+          userData?.Role === "AIRLINE_ADMIN") &&
           !businessData)
       )
         await fetchUserAndBusinessData();
@@ -63,11 +63,11 @@ function BaseHomePage() {
       {/* NavBar */}
       <Navbar />
       {/* HomePage Based on Role */}
-      {userData.role === "HOTEL_ADMIN" ? (
+      {userData?.role === "HOTEL_ADMIN" ? (
         <HotelHomePage />
-      ) : userData.role === "AIRLINE_ADMIN" ? (
+      ) : userData?.role === "AIRLINE_ADMIN" ? (
         <AirlineHomePage />
-      ) : userData.role === "SYSTEM_ADMIN" ? (
+      ) : userData?.role === "SYSTEM_ADMIN" ? (
         <SystemAdminHomePage />
       ) : (
         <div className="p-8">
