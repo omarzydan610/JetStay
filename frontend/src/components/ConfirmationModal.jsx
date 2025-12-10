f07076749bc0ba4632a04d568aa8ee39b3f8bf89
joe <engjo1010@gmail.com>
2025-12-10 13:36:44 +0200

    fixing pug for CI

--- frontend/src/components/ConfirmationModal.jsx
+++ frontend/src/components/ConfirmationModal.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { AlertTriangle, X } from "lucide-react";
+import { AlertTriangle} from "lucide-react";
 
 const ConfirmationModal = ({ isOpen, onClose, onConfirm, title, message }) => {
   if (!isOpen) return null;
