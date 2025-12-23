1e9da6d1664751b11d6ee8993634ef5c507dbf3e
AbdElrahman Elsayed <abdonawar1234567@gmail.com>
2025-12-24 00:31:51 +0200

    feat: Implement room booking functionality with new controller, service, DTOs, and repository updates.

--- backend/src/main/java/com/example/backend/repository/RoomBookingRepository.java
+++ backend/src/main/java/com/example/backend/repository/RoomBookingRepository.java
@@ -1,9 +1,18 @@
 package com.example.backend.repository;
 
+import com.example.backend.entity.Hotel;
 import com.example.backend.entity.RoomBooking;
+
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomBookingRepository extends JpaRepository<RoomBooking, Integer> {
+    @Query("SELECT SUM(rb.noOfRooms) FROM RoomBooking rb WHERE rb.roomType.roomTypeID = :roomTypeID AND (rb.checkIn < :checkOut OR rb.checkOut > :checkIn)")
+    public Integer getNumberOfBookedRoom(LocalDate checkIn , LocalDate checkOut , Integer roomTypeID);
+
+    
 }
---
+++ backend/src/main/java/com/example/backend/service/BookingService/RoomBookingService.java
@@ -0,0 +1,93 @@
+package com.example.backend.service.BookingService;
+
+
+
+import java.time.LocalDate;
+import com.example.backend.entity.User;
+import com.example.backend.entity.Hotel;
+import jakarta.transaction.Transactional;
+import com.example.backend.entity.RoomType;
+import com.example.backend.entity.RoomBooking;
+import org.springframework.stereotype.Service;
+import com.example.backend.repository.UserRepository;
+import com.example.backend.entity.BookingTransaction;
+import com.example.backend.repository.HotelRepository;
+import com.example.backend.exception.BadRequestException;
+import com.example.backend.repository.RoomTypeRepository;
+import com.example.backend.repository.RoomBookingRepository;
+import com.example.backend.dto.BookingDTOs.CreateBookingDTO;
+import com.example.backend.dto.BookingDTOs.RoomTypeBookingDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.backend.repository.BookingTransactionRepository;
+
+
+@Service
+public class RoomBookingService {
+    @Autowired
+    RoomBookingRepository roomBookingRepository;
+
+    @Autowired
+    RoomTypeRepository roomTypeRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    HotelRepository hotelRepository;
+
+    @Autowired
+    BookingTransactionRepository bookingTransactionRepository;
+
+    @Transactional
+    public Integer bookingService(CreateBookingDTO createBookingDTO, Integer user_id) {
+        User user = userRepository.getByUserID(user_id);
+        Hotel hotel = hotelRepository.getByHotelID(createBookingDTO.getHotelID());
+        LocalDate checkIn = createBookingDTO.getCheckIn();
+        LocalDate checkOut = createBookingDTO.getCheckOut();
+        int noOfGuests = createBookingDTO.getNoOfGuests();
+        BookingTransaction bookingTransaction = getBookingTransaction(user, hotel, checkIn, checkOut, noOfGuests);
+        for (RoomTypeBookingDTO roomTypeBookingDTO : createBookingDTO.getRoomTypeBookingDTO()) {
+            bookRoom(roomTypeBookingDTO, user, hotel, checkIn, checkOut, bookingTransaction);
+        }
+        return bookingTransaction.getBookingTransactionId();
+    }
+
+    private BookingTransaction getBookingTransaction(User user, Hotel hotel, LocalDate checkIn, LocalDate checkOut,
+            int noOfGuests) {
+        BookingTransaction bookingTransaction = new BookingTransaction();
+        bookingTransaction.setUser(user);
+        bookingTransaction.setHotel(hotel);
+        bookingTransaction.setCheckIn(checkIn);
+        bookingTransaction.setCheckOut(checkOut);
+        bookingTransaction.setNumberOfGuests(noOfGuests);
+        bookingTransactionRepository.save(bookingTransaction);
+        return bookingTransaction;
+    }
+
+    private void bookRoom(RoomTypeBookingDTO roomTypeBookingDTO, User user, Hotel hotel, LocalDate checkIn,
+            LocalDate checkOut, BookingTransaction bookingTransaction) {
+        RoomType roomType = roomTypeRepository.lockRoomType(roomTypeBookingDTO.getRoomTypeID());
+        Integer bookedRoomCount = roomBookingRepository.getNumberOfBookedRoom(checkIn, checkOut,
+                roomTypeBookingDTO.getRoomTypeID());
+        int noOfBookedRoom = (bookedRoomCount == null) ? 0 : bookedRoomCount;
+        int noOfAvailableRoom = roomType.getQuantity() - noOfBookedRoom;
+        if (noOfAvailableRoom < roomTypeBookingDTO.getNoOfRooms()) {
+            throw new BadRequestException("this number of rooms is Not available ");
+        }
+        saveTheRoomBooking(roomTypeBookingDTO, user, hotel, checkIn, checkOut, roomType, bookingTransaction);
+    }
+
+    private void saveTheRoomBooking(RoomTypeBookingDTO roomTypeBookingDTO, User user, Hotel hotel, LocalDate checkIn,
+            LocalDate checkOut, RoomType roomType, BookingTransaction bookingTransaction) {
+        RoomBooking roomBooking = new RoomBooking();
+        roomBooking.setUser(user);
+        roomBooking.setHotel(hotel);
+        roomBooking.setRoomType(roomType);
+        roomBooking.setCheckIn(checkIn);
+        roomBooking.setCheckOut(checkOut);
+        roomBooking.setNoOfRooms(roomTypeBookingDTO.getNoOfRooms());
+        roomBooking.setBookingTransaction(bookingTransaction);
+        roomBookingRepository.save(roomBooking);
+    }
+
+}
