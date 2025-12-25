27443a7540fc64d5244373bf9d42aa481fd7ed81
AbdElrahman Elsayed <abdonawar1234567@gmail.com>
2025-12-25 03:01:00 +0200

    chore: Update database connection string to use root user for local development

--- microservice/payment/app/database/database.py
+++ microservice/payment/app/database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://Hema:2004@localhost:3306/JetStay"
+DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/JetStay"
 
 engine = create_engine(
     DATABASE_URL,
